12e21ced598819b5997d82e833d7166659df4fe0
Samson Keflinkiel <samsonkeflinkiel.edits@gmail.com>
2025-11-17 21:18:46 -0500

    NewestVersion

--- Assets/Scripts/LazerScript.cs
+++ Assets/Scripts/LazerScript.cs
@@ -3,28 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class Laser : MonoBehaviour
 {
-    public float speed = 40f;    // Laser speed
-    public float lifeTime = 3f;  // Destroy after this time
+    public float speed = 40f;            // Laser speed
+    public float lifeTime = 3f;          // Destroy after time
+    public Vector2 shootDirection = Vector2.right;  // <-- Editable direction
 
     private Vector3 moveDirection;
 
     void Start()
     {
-        
-        moveDirection = transform.right.normalized;
+        // Use the direction chosen in the Inspector  
+        moveDirection = shootDirection.normalized;
 
         Destroy(gameObject, lifeTime);
     }
 
     void Update()
     {
-        
         transform.position += moveDirection * speed * Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("player"))   
+        if (other.CompareTag("player"))
         {
             SceneManager.LoadScene(1);
         }
